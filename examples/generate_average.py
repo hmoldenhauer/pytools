7faa7211ff0abd70806c304774b34d2f306a88a1
hmoldenhauer <henning.moldenhauer@udo.edu>
2018-03-28 11:22:01 +0200

    added legend

--- examples/generate_average.py
+++ examples/generate_average.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# import files from pytools
+# import files from pytools has to be modified if used elsewhere
 import sys
 sys.path.insert(0, '/home/henning/repos/promotion/pytools')
 
@@ -31,22 +31,23 @@ frame = 0
 listOfFiles = read.get_folder_content(foldername, 'txt')
 x, y_raw = read.get_mono_data(listOfFiles)
 
-# remove background
-y = remove.remove_median(y_raw)
-
 # remove muons
-indexes = remove.find_muons(y, threshold, halfwidth)
-y_muon_free = remove.remove_muons(y, indexes, muonsPerFrame, halfwidth, sensitivity)
+indexes = remove.find_muons(y_raw, threshold, halfwidth)
+y_muon_free = remove.remove_muons(y_raw, indexes, muonsPerFrame, halfwidth, sensitivity)
+
+# remove background
+y = remove.remove_median(y_muon_free)
 
 # calculate average spectrum and set minimal value to zero
-y_average = np.average(y_muon_free, axis=0)
+y_average = np.average(y, axis=0)
 y_average = y_average - min(y_average)
 
 data = np.vstack((x[0], y_average)).T
 np.savetxt(savename + 'average.csv', data, '%1.2f')
 
-plt.plot(x[frame], y_raw[frame])
-plt.plot(x[frame], y_muon_free[frame])
-plt.plot(x[0], y_average)
+plt.plot(x[frame], y_raw[frame], label='raw data')
+plt.plot(x[frame], y_muon_free[frame], label='muons removed')
+plt.plot(x[0], y_average, label='average')
+plt.legend()
 
 plt.show()
